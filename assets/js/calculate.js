97b654cea7f3238c73324fa73c1145fbb716e5ab
William Chiang <45242555+WillBillNye@users.noreply.github.com>
2019-05-14 22:44:44 -0400

    Update

---
+++ assets/js/calculate.js
@@ -0,0 +1,31 @@
+var categories = document.getElementById("Categories")
+if (categories != null){
+    var descrip = categories.getElementsByClassName("description")
+    var arrPoints = new Array(descrip.length)
+    for (i = 0; i < descrip.length; i++) {
+        let temp = new Array(3)
+        temp[0] = descrip[i].getElementsByClassName("numeric")[0].getElementsByClassName("points")[0].innerHTML
+        temp[1] = descrip[i].getElementsByClassName("numeric")[0].getElementsByClassName("text-muted")[0].innerHTML
+        temp[2] = descrip[i].getElementsByClassName("percent")[0].innerHTML
+        temp[2] = temp[2].substring(0, temp[2].indexOf("%"))
+        temp[3] = descrip[i].innerHTML
+        temp[3] = temp[3].substring(temp[3].indexOf("\n"), temp[3].indexOf("<"))
+        temp[3] = temp[3].trim(" ")
+        console.log(temp[0] + " " + temp[1] + " " + temp[2] + " " + temp[3])
+        arrPoints[i] = temp
+    }
+} else {
+    var assignments = document.getElementsByClassName("assignment")
+    var received = 0
+    var total = 0
+    for (i = 0; i < assignments.length; i++) {
+        let rTemp = assignments[i].getElementsByClassName("points")[0].innerHTML
+        if (!(rTemp === (""))) {
+            let tTemp = assignments[i].getElementsByClassName("max")[0].innerHTML
+            tTemp = tTemp.substring(tTemp.indexOf(" "), tTemp.length)
+            received = received + parseInt(rTemp)
+            total = total + parseInt(tTemp)
+        }
+    }
+    console.log(received + " " + total)
+}
