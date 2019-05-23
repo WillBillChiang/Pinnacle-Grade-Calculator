0221d2c8221d7e4627d16470056ba3a18ddd35a3
William Chiang <45242555+WillBillChiang@users.noreply.github.com>
2019-05-22 21:02:23 -0400

    Update

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,12 +1,31 @@
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs){
     var url = String(tabs[0].url)
+    console.log("test1")
     if (url.includes("://gb.browardschools.com/Pinnacle/Gradebook/InternetViewer/StudentAssignments")){
         chrome.storage.local.get(['category'], function(result) {
-            if (result.cat) {
+            if (result.category == true) {
                 
+                function calc(arr) {
+                    let res = 0
+                    for (let i = 0; i < arr.length; i++) {
+                        res += (parseFloat(arr[i][0])/parseFloat(arr[i][1]))*(parseFloat(arr[i][2])/100)
+                        console.log(res)
+                    }
+                    console.log(res)
+                    return res
+                }
+
+                chrome.storage.local.get(['catInfo'], function(result) {
+                    console.log(result.catInfo)
+                    var displayGrade = calc(result.catInfo)
+                    console.log(displayGrade)
+                } )
+
             } else {
 
             }
         })
+    } else {
+        //GO TO PINNACLE GRADE PAGE
     }
 })
