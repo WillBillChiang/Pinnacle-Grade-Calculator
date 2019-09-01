ff772b7dc3febf374e666979d15d9a379eeefbf2
William Chiang <45242555+WillBillChiang@users.noreply.github.com>
2019-08-31 22:15:09 -0400

    Removes Unnecessary Permsions

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(activeTab){
     var newURL = "https://gb.browardschools.com/Pinnacle/Gradebook/Default.aspx";
-    chrome.tabs.create({ url: newURL });
+    window.open(newURL)
   });
